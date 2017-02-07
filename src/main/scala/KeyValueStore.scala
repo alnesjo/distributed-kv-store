574b9e98851b2f39849f7447bc4bb4b88582f2e9
Robert Alnesjö <alnesjo@kth.se>
2017-02-07 12:36:33 +0100

    Boostrap

--- src/main/scala/KeyValueStore.scala
+++ src/main/scala/KeyValueStore.scala
@@ -1,41 +1,50 @@
+import se.sics.kompics.network.Network
 import se.sics.kompics.sl._
 import se.sics.kompics.{Kompics, KompicsEvent, Start, Init => JInit}
-//import se.sics.kompics.network.{Network}
+import se.sics.kompics.network.{Network}
 
 sealed trait KeyValueStoreEvent extends KompicsEvent
 case class Get(key: String) extends KeyValueStoreEvent // TODO allow keys and values other than strings
 case class Store(key: String, value: String) extends KeyValueStoreEvent
 case class Post(key: String, value: String) extends KeyValueStoreEvent
 
-// Borrowing some ideas from the Java code skeleton which is confusing for now
-case class RouteMessage(key: String, message: KompicsEvent) extends KompicsEvent
-
-object RoutePort extends Port {
-  request[RouteMessage]
-  indication[RouteMessage]
-}
-
 class KeyValueStore extends ComponentDefinition {
-  val input = requires(RoutePort)
-  val output = provides(RoutePort)
-
-  input uponEvent {
-    case RouteMessage("This key is probably unrelated to the storage", Get(key)) => handle {
-      ???
-    }
-    case Get(key) => handle {
-      // Something along the lines of
-      //   if this node contains key, then post it's value back
-      //   otherwise ask someone else
-      //   perhaps get-requests are multicast to begin with?
-      trigger(RouteMessage("key?", Post(key, "Some value fetched from storage")), output)
-      ???
-    }
-    case Store(key, value) => handle {
-      ???
-    }
-    case Post(key, value) => handle {
-      ???
+  val input = requires[Network]
+  val output = provides[Network]
+  ctrl uponEvent {
+    case _: Start => handle {
+      println("Requesting value at A")
+      trigger(Get("A"), output)
     }
   }
+  input uponEvent {
+    case Get("A") => handle {
+      trigger(Post("A", "apple"), output)
+    }
+    case Get("next key") => handle {
+    }
+    case Get(key) => handle {
+      trigger(Post(key, "some fruit"), output)
+    }
+    case Store(key, value) => handle {
+    }
+    case Post("A", value) => handle {
+      println("Requesting value at B")
+      trigger(Get("B"), output)
+    }
+    case Post(key, value) => handle {
+      println("Requesting value at next key")
+      trigger(Get("next key"), output)
+    }
+  }
+}
+
+class KeyValueStoreService extends ComponentDefinition {
+  val boot = create(classOf[KeyValueStore], JInit.NONE)
+  connect[Network](boot -> boot)
+}
+
+object Main2 extends App {
+  Kompics.createAndStart(classOf[KeyValueStoreService])
+  Kompics.waitForTermination()
 }
