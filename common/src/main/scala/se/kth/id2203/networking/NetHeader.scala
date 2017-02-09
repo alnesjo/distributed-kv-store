8f3452b2965a89bbd678629a4c361cca60b3bfc1
Robert Alnesjö <alnesjo@kth.se>
2017-02-09 16:13:54 +0100

    Case classes are pretty nice

--- common/src/main/scala/se/kth/id2203/networking/NetHeader.scala
+++ common/src/main/scala/se/kth/id2203/networking/NetHeader.scala
@@ -5,7 +5,7 @@ import se.sics.kompics.network.{Header, Transport}
 /**
   * Created by robert on 2017-02-09.
   */
-class NetHeader(val src: NetAddress, val dst: NetAddress, val ptc: Transport) extends Header[NetAddress] {
+case class NetHeader(val src: NetAddress, val dst: NetAddress, val ptc: Transport) extends Header[NetAddress] {
   override def getSource = src
   override def getDestination = dst
   override def getProtocol = ptc
--- common/src/main/scala/se/kth/id2203/networking/NetMessage.scala
+++ common/src/main/scala/se/kth/id2203/networking/NetMessage.scala
@@ -3,8 +3,8 @@ package se.kth.id2203.networking
 import se.sics.kompics.KompicsEvent
 import se.sics.kompics.network.{Msg, Transport}
 
-class NetMessage(src: NetAddress, dst: NetAddress, val payload: KompicsEvent) extends Msg[NetAddress, NetHeader] with Serializable {
-  val header = new NetHeader(src, dst, Transport.TCP)
+case class NetMessage(src: NetAddress, dst: NetAddress, payload: KompicsEvent) extends Msg[NetAddress, NetHeader] with Serializable {
+  val header = NetHeader(src, dst, Transport.TCP)
   override def getHeader = header
   override def getSource = header.getSource
   override def getDestination = header.getDestination
