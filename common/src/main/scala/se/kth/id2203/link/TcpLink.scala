ad85bfcfd1838019554d2b4167447dde114bca51
Robert Alnesjö <alnesjo@kth.se>
2017-02-20 18:22:41 +0100

    Debbuging settings

--- common/src/main/scala/se/kth/id2203/link/TcpLink.scala
+++ common/src/main/scala/se/kth/id2203/link/TcpLink.scala
@@ -1,5 +1,6 @@
 package se.kth.id2203.link
 
+import org.slf4j.LoggerFactory
 import se.kth.id2203.{PL_Deliver, PL_Send, PerfectLink}
 import se.sics.kompics.network.{Address, Network, Transport}
 import se.sics.kompics.sl._
@@ -12,19 +13,23 @@ object TcpLink {
 
 class TcpLink(init: TcpLink.Init) extends ComponentDefinition {
 
+  val log = LoggerFactory.getLogger(classOf[TcpLink])
+
   val pl = provides(PerfectLink)
   val net = requires[Network]
 
   val self = init.self
 
   pl uponEvent {
-    case PL_Send(dst, payload) => handle {
+    case e@PL_Send(dst, payload) => handle {
+      log.debug(s"Handling request $e on PerfectLink")
       trigger(NetworkMessage(self, dst, Transport.TCP, payload) -> net)
     }
   }
 
   net uponEvent {
-    case NetworkMessage(src, `self`, Transport.TCP, payload) => handle {
+    case e@NetworkMessage(src, `self`, Transport.TCP, payload) => handle {
+      log.debug(s"Handling request $e on Network")
       trigger(PL_Deliver(src, payload) -> pl)
     }
   }
