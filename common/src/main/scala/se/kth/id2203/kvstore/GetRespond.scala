58735e3a4d5edf3ced5fc1bff1d2b4ba39f14537
Robert Alnesjö <alnesjo@kth.se>
2017-03-03 01:21:33 +0100

    Typo

--- common/src/main/scala/se/kth/id2203/kvstore/GetRespond.scala
+++ common/src/main/scala/se/kth/id2203/kvstore/GetRespond.scala
@@ -2,4 +2,4 @@ package se.kth.id2203.kvstore
 
 import se.kth.id2203.overlay.Identifier
 
-case class GetRespond(id: Identifier, value: Option[String], status: Code = Okop) extends Response
+case class GetRespond(id: Identifier, value: Option[String], status: Code = Ok) extends Response
