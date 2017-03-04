f5df2d0a4e73c7ada2b8a9c5fd98204bfa9fc689
Robert Alnesjö <alnesjo@kth.se>
2017-03-04 12:07:44 +0100

    Exclusion set for node failures

--- server/src/main/scala/se/kth/id2203/overlay/LookupTable.scala
+++ server/src/main/scala/se/kth/id2203/overlay/LookupTable.scala
@@ -5,33 +5,36 @@ import se.sics.kompics.network.Address
 
 object LookupTable {
 
-  def generate(nodes: Set[Address], replicationDegree: Int): LookupTable = {
+  def generate(nodes: Set[Address], rpd: Int): LookupTable = {
     val (k, n) = nodes
       .map(a => (a.##, a))
       .toList
       .unzip
-    val partitions = (0 until replicationDegree)
+    val partitions = (0 until rpd)
       .map(i => k zip ((n drop i) ++ (n take i)))
       .reduce(_ ++ _)
       .groupBy(_._1)
       .mapValues(_ map (_._2))
       .mapValues(_ toSet)
       .map(identity) // https://issues.scala-lang.org/browse/SI-7005
-    LookupTable(partitions)
-
+    LookupTable(partitions, Set.empty)
   }
 
 }
 
-case class LookupTable(partitions: Map[Int, Set[Address]]) {
+case class LookupTable(partitions: Map[Int, Set[Address]], exclude: Set[Address]) {
 
   val log = LoggerFactory.getLogger(classOf[LookupTable])
 
   def lookup(key: Any): Set[Address] = partitions.keys.filter(_ <= key.##).lastOption match {
-    case Some(partition) => partitions(partition)
-    case None => partitions.values.last
+    case Some(partition) => partitions(partition).filterNot(exclude)
+    case None => partitions.values.last.filterNot(exclude)
   }
 
-  def getNodes: Set[Address] = partitions.values.reduce(_ ++ _)
+  def getNodes: Set[Address] = partitions.values.reduce(_ ++ _).filterNot(exclude)
+
+  def suspect(address: Address): Unit = exclude += address
+
+  def restore(address: Address): Unit = exclude -= address
 
 }
